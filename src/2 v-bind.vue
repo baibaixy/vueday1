a9f7ad98944a285fe72e3365be195b6b48948881
baibaixy <2942583916@qq.com>
2022-06-28 20:06:35 +0800

    v-bind

---
+++ src/2 v-bind.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <a v-bind:href="url">去百度</a>
+    <img :src="imgURl" alt="" />
+    <img :src="localImg" alt="" />
+  </div>
+</template>
+
+<script>
+import imgObj from './assets/logo.png';
+export default {
+  data() {
+    return {
+      url: 'http://www.baidu.com',
+      imgURl:
+        'http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg',
+      localImg: imgObj,
+    };
+  },
+};
+</script>
+
+<style></style>
