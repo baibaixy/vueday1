f02bb9e479b87b1d4fce22edcc4b013f06577b9d
baibaixy <2942583916@qq.com>
2022-06-28 19:57:20 +0800

    插值语法

---
+++ src/1 插值语法.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>{{ obj.name }}</h2>
+    <h3>{{ obj.age >= 18 ? '成年' : '未成年' }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'HelloWorld',
+      obj: {
+        name: 'vv',
+        age: 18,
+      },
+    };
+  },
+};
+</script>
+
+<style scoped></style>
