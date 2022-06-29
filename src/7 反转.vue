79e76e3aebdfb089da8162d7c74bc04f3e496320
baibaixy <2942583916@qq.com>
2022-06-29 13:58:56 +0800

    ending

---
+++ src/7 反转.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <button @click="btn">反转</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello,World',
+    };
+  },
+  methods: {
+    btn() {
+      this.msg = this.msg.split('').reverse().join('');
+    },
+  },
+};
+</script>
+
+<style></style>
